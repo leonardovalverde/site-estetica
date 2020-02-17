3066ca672405cd15386d61e001c103dcb8e8b4c3
Leonardo Valverde <31359971+leonardovalverde@users.noreply.github.com>
2020-02-17 19:25:19 -0300

    First Commit (e único)
    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+new SimpleSlide({
+  slide: "principal", // nome do atributo data-slide="principal"
+  nav: false, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 7000, // tempo de transição dos slides
+  pauseOnHover: true // pausa a transição automática
+});
+
+if(window.SimpleAnime){
+new SimpleAnime();
+}
+
+if(window.SimpleForm){
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email leovmsi@hotmail.com.</p></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+}); 
+}
